40c228518941c53e660d6e2bb7c6e1fd4cdf8bab
SBlera <70284798+SBlera@users.noreply.github.com>
2020-09-23 18:45:33 +0300

    Delete test.java

--- src/test/java/test.java
+++
@@ -1,8 +0,0 @@
-import org.openqa.selenium.By;
-
-public class test {
-    public static void main(String[] args) {
-        StartAndNavigate.RunStartAndNavigate();
-        SingeltonDriver.getDriverInstance().findElement(By.xpath("//*[@id=\"ember714\"]/div/ul[1]/li[2]/a")).click();
-    }
-}
